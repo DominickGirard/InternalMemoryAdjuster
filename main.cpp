3217332050f4a7c06bd946a1d0d559ff3efb7870
Dominick Girard <dominickgirard112@gmail.com>
2023-09-23 23:47:27 -0700

    window dll injector

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <Windows.h>
+#include <iostream>
 
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) {
 	MessageBox(0, 0, 0, 0);
-
+	std::cout << "yo";
 	return true;
 }
