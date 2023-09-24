b3c81c8cbc60779944180417eee7a688fef47434
Dominick Girard <dominickgirard112@gmail.com>
2023-09-24 00:11:10 -0700

    started thread in process and checks for m key pressed using windows api

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
 #include <Windows.h>
 #include <iostream>
 
+
+
+void injected_thread() {
+	while (true) {
+		// GetAsyncKeyState will constantly return true while key is held down
+		if (GetAsyncKeyState(0x4D)) {  // 0x4D = m
+			
+		}
+		Sleep(1);
+	}
+}
+
+
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) {
-	MessageBox(0, 0, 0, 0);
-	std::cout << "yo";
+	if (fdwReason == DLL_PROCESS_ATTACH)
+	{
+		CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)injected_thread, NULL, 0, NULL);
+	}
 	return true;
 }
